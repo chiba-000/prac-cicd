b4839ac9837b3f3b4ec921d7ff007cdbc7a45a47
chiba <chiba.toranosuke@commercecrew.jp>
2026-01-05 12:35:18 +0900

    デプロイテスト

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <p>Hello World</p>
+      <p>CI/CD Practice</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
